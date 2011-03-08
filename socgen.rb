77ff5668425147e760996411275f0395273f1dd9
Sidney Burks <sid137@gmail.com>
2011-03-08 12:31:41 +0100

    'GET /call_response' correctly renders payment form

--- socgen.rb
+++ socgen.rb
@@ -1,9 +1,44 @@
 require 'sinatra'
 require 'haml'
+require 'yaml'
+
+request_binary  = 'plugin/bin/request'
+response_binary = 'plugin/bin/response'
+
+# For each key and value in the options hash, it rejects the keys with empty
+# values, creates the string "key=value" for each k,v pair, 
+# and joins them via a space
+def parameter_string opts
+  opts.reject{|k,v| v.nil? }.collect{ |k,v| "#{k}=#{v}"}.join(' ')
+end
 
 
 get '/' do
   haml :index
+end
+
+get '/call_request' do
+  socgen_params = YAML.load_file('test_params.yaml')
+  # merge user-level params in nhere with defaulss
+  request_params = parameter_string(socgen_params)
+  result = `#{request_binary} #{request_params}`
+  null, code, error, message = result.split('!')
+  if (code.empty? && error.empty?)
+    "Error calling request. Executable not found"
+  elsif (code != "0")
+    "Error calling the payment API.  Error message #{error}"
+  else
+    haml :call_request, :locals => {:error => error, :message => message}
+  end
+end
+
+post '/call_response' do
+
+end
+
+
+post '/call_autoresponse' do
+
 end
 
 
@@ -11,9 +46,17 @@ __END__
 @@ layout
 %html
   %head
-    %title Socgenactif Test
+    %title SOGENACTIF - Paiement Securise sur Internet
   %body
     = yield
 
 @@ index
-  Hi, we're up
+This is the test app for SocgenActif in Ruby
+%p
+  %a{:href => url('/call_request')} Call Request (Confirm Payment)
+
+
+@@ call_request
+= error
+= message
+
